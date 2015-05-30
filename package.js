acabef6df12fe49c4899bcf7fb5dc5395225c32c
dfischer <me@danielfischer.com>
2015-05-29 19:29:27 -0700

    try loading html file

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Port from digilord to allow on client",
     name: "dfischer:faker",
-    version: "1.0.7",
+    version: "1.0.8",
     git: "https://github.com/dfischer/meteor-faker"
 });
 
@@ -10,8 +10,9 @@ Npm.depends({"faker": "2.1.2"});
 Package.onUse(function (api) {
   api.versionsFrom('1.0');
   api.use('coffeescript');
-  api.add_files('faker.coffee', 'server');
+  api.add_files('dfischer:faker.coffee', 'server');
   api.add_files('build/faker.js', 'client');
+  api.add_files('dfischer:faker.html', 'client');
 
   api.export('faker', 'server');
 });
